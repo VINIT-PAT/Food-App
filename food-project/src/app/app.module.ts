49e2ed63785445effe7e83850c664423ab5236a1
VINIT-PAT <144584021+VINIT-PAT@users.noreply.github.com>
2023-10-30 07:03:10 +0000

    Header

--- food-project/src/app/app.module.ts
+++ food-project/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { CarousalComponent } from './carousal/carousal.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarousalComponent
   ],
   imports: [
     BrowserModule,
